5978c04cc3d05a8075ad773a9a47447755f3d4b1
ruidantas <ruifisicauesb@hotmail.com>
2023-01-16 14:54:11 -0300

    chore: header

---
+++ yugioh-react-api/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Home__header Header">
+      <div className="Header__logo Logo">
+      </div>
+    </div>
+  );
+}
+
+export { Header };
