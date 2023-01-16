c83beb6cc254ac4ccbd6f6542af67a8a4e33a90c
ruidantas <ruifisicauesb@hotmail.com>
2023-01-16 14:48:38 -0300

    chore:  form

---
+++ yugioh-react-api/src/components/forms/form.jsx
@@ -0,0 +1,151 @@
+import "./form.css";
+import { useState } from "react";
+import { api } from "../../helpers/Api.js";
+
+export function FormCreate({ closeModal, createCard }) {
+  const [newCard, setNewCard] = useState([]);
+
+  async function formsSubmit(event) {
+    event.preventDefault();
+
+    const renameImage = (img) => img.split("\\").pop();
+    const { name, typing, level, category, atributte, effect, image, atk, def } = newCard;
+
+    const card = {
+      name,
+      typing,
+      level,
+      category,
+      atributte,
+      effect,
+      image: `assets/images/${renameImage(image)}`,
+      atk,
+      def
+    };  
+
+    const response = await api.createNewCard(card);
+    createCard(response);
+    closeModal();
+  }
+
+  return (
+    <div className="form">
+      <form autoComplete="off" onSubmit={formsSubmit} className="form__card">
+        <section>
+          <span>Name: </span>
+          <input
+            type="text"
+            name="name"
+            required
+            onChange={(event) => {
+              setNewCard({ ...newCard, name: event.target.value });
+            }}
+          ></input>
+        </section>
+
+        <section>
+          <span>Typing: </span>
+          <input
+            type="text"
+            name="name"
+            required
+            onChange={(event) => {
+              setNewCard({ ...newCard, typing: event.target.value });
+            }}
+          ></input>
+        </section>
+
+        <section>
+          <span>Level: </span>
+          <input
+            type="number"
+            name="level"
+            required
+            onChange={(event) => {
+              setNewCard({ ...newCard, level: event.target.value });
+            }}
+          ></input>
+        </section>
+
+        <section>
+          <span>Category: </span>
+          <input
+            type="text"
+            name="category"
+            required
+            onChange={(event) => {
+              setNewCard({ ...newCard, category: event.target.value });
+            }}
+          ></input>
+        </section>
+        <section>
+          <span>Atributte: </span>
+          <input
+            type="text"
+            name="category"
+            required
+            onChange={(event) => {
+              setNewCard({ ...newCard, atributte: event.target.value });
+            }}
+          ></input>
+        </section>
+
+        <section>
+          <span>Effect: </span>
+          <input
+            type="text"
+            name="category"
+            required
+            onChange={(event) => {
+              setNewCard({ ...newCard, effect: event.target.value });
+            }}
+          ></input>
+        </section>
+
+        <section>
+          <span>Image: </span>
+          <input
+            type="file"
+            accept="image/png, image/jpeg, image/jpg, image/gif, image/webp"
+            alt="image"
+            onChange={(event) => {
+              setNewCard({
+                ...newCard,
+                image: event.target.value,
+              });
+            }}
+          ></input>
+        </section>
+
+        <section>
+          <span>ATK: </span>
+          <input
+            type="text"
+            name="category"
+            required
+            onChange={(event) => {
+              setNewCard({ ...newCard, atk: event.target.value });
+            }}
+          ></input>
+        </section>
+
+        <section>
+          <span>DEF: </span>
+          <input
+            type="text"
+            name="category"
+            required
+            onChange={(event) => {
+              setNewCard({ ...newCard, def: event.target.value });
+            }}
+          ></input>
+        </section>
+
+
+        <button type="submit" className="submit">
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+}
