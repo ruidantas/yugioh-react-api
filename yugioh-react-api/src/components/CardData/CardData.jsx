1da57897a4dfb28c8f7b32891076ad4b2c53ed33
ruidantas <ruifisicauesb@hotmail.com>
2023-01-16 14:49:26 -0300

    chore: card data

---
+++ yugioh-react-api/src/components/CardData/CardData.jsx
@@ -0,0 +1,19 @@
+import "./CardData.css";
+
+function CardData({ typing, level, category, atributte, effect, image, atk, def }) {
+  return (
+    <div className="modal-info">
+      <div className="modal-info">{typing}</div>
+      <div className="modal-info">{level}</div>
+      <div className="modal-info">{category}</div>
+      <div className="modal-info">{atributte}</div>
+      <div className="modal-info">{effect}</div>
+      <img className="modal-info" src={image} />
+      <div className="modal-info">{atk}</div>
+      <div className="modal-info">{def}</div>
+
+    </div>
+  );
+}
+
+export { CardData };
